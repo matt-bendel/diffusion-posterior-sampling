066d48e39ebb660ce3a61dcdb89e7e2b2cd62491
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-04 11:37:32 -0400

    Moved notebook to script

--- guided_diffusion/gaussian_diffusion_reform.py
+++ guided_diffusion/gaussian_diffusion_reform.py
@@ -188,11 +188,8 @@ class GaussianDiffusion:
         for idx in pbar:
             time = torch.tensor([idx] * img.shape[0], device=device)
 
-            next_im = extract_and_expand(self.rho_t, time, img) * img
-            next_im += extract_and_expand(self.xi_t, time, img) * self.denoise(x=img, t=time, model=model, y=measurement, forward_model=forward_model)['pred_xstart']
-            next_im += extract_and_expand(self.sigma_t, time, img) * torch.randn_like(img)
-
-            img = next_im
+            img = extract_and_expand(self.rho_t, time, img) * img + extract_and_expand(self.xi_t, time, img) * self.denoise(x=img, t=time, model=model, y=measurement, forward_model=forward_model)['pred_xstart'] + extract_and_expand(self.sigma_t, time, img) * torch.randn_like(img)
+            img = img.detach()
 
             if record:
                 if idx % 50 == 0:
