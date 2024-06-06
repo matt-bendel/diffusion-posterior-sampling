7e46bc5f7e5e3aa3ae46be607d0c16ff17a41422
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-06 12:21:29 -0400

    VAMP stuff...

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -27,8 +27,8 @@ class VAMP:
         diff = torch.abs(noise_var - (1 - torch.tensor(self.alphas_cumprod).to(noisy_im.device)))
         nearest_indices = torch.argmin(diff, dim=1)
 
-        # t = nearest_indices.repeat(noisy_im.shape[0])
-        # print(f't = {t[0]}')
+        tmp = nearest_indices.repeat(noisy_im.shape[0]) # t
+        print(f't = {tmp[0]}')
         # t_alpha_bar = extract_and_expand(self.alphas_cumprod, t, noisy_im)[0, 0, 0, 0]
 
         scale_factor_prime = torch.sqrt((1 - t_alpha_bar) / noise_var)
