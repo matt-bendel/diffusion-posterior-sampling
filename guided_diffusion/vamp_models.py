98a1913cf59efc345a633635cfffc0c5741a20ed
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-11 18:44:36 -0400

    Noise update

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -7,7 +7,7 @@ class VAMP:
         self.max_iters = max_iters
         self.K = K
         self.delta = 1e-4
-        self.damping_factor = 0.2 # Factor for damping (per Saurav's suggestion)
+        self.damping_factor = 0.5 # Factor for damping (per Saurav's suggestion)
 
         self.betas = torch.tensor(betas).to(x_T.device)
         self.gamma_1 = 1e-6 * torch.ones(x_T.shape[0], 1, device=x_T.device)
