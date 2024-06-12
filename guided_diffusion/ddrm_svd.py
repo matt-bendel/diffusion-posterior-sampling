bc55c5b4c9b6241ba5811e81404c8536fb4edc57
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-12 15:00:27 -0400

    Noise update

--- guided_diffusion/ddrm_svd.py
+++ guided_diffusion/ddrm_svd.py
@@ -62,10 +62,7 @@ class H_functions:
 
     def vamp_mu_1(self, vec, sig_y, sig_ddpm, gamma_1):
         temp = self.Vt(vec)
-        if len(self.singular().shape) == 1:
-            evals = self.add_zeros((self.singulars().unsqueeze(0).repeat(vec.shape[0], 1) / sig_y) ** 2).T
-        else:
-            evals = self.add_zeros((self.singulars() / sig_y) ** 2).T.repeate(vec.shape[0], 1)
+        evals = self.add_zeros((self.singulars().unsqueeze(0).repeat(vec.shape[0], 1) / sig_y) ** 2)
         temp = ((evals + sig_ddpm ** 2 + gamma_1[:, 0, None]) ** -1) * temp
         return self.V(temp)
 
