c9e28cc7ba6ad0d34112e8441311cbd91102fc26
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-12 17:07:49 -0400

    Noise update

--- sample_condition_new.py
+++ sample_condition_new.py
@@ -72,7 +72,7 @@ def main():
     cond_config = task_config['conditioning']
     cond_config['params']['scale'] = 2.0
     # cond_method = get_conditioning_method(cond_config['method'], operator, noiser, **cond_config['params'])
-    # measurement_cond_fn = cond_method.conditioning
+    measurement_cond_fn = None #cond_method.conditioning
     logger.info(f"Conditioning method : {task_config['conditioning']['method']}")
 
     # Load diffusion sampler
