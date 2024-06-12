e9343a072960be98b370e7ca8227926e0e45d665
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-12 13:54:03 -0400

    Noise update

--- guided_diffusion/gaussian_diffusion_reform.py
+++ guided_diffusion/gaussian_diffusion_reform.py
@@ -192,7 +192,7 @@ class GaussianDiffusion:
         # ones = torch.ones(x_start.shape).to(x_start.device)
         # vamp_model = Inpainting(model, self.betas, self.alphas_cumprod, 1, x_start, ones * mask, ones * (1 - mask))
         # vamp_model = Deblur(model, self.betas, self.alphas_cumprod, 1, x_start, torch.Tensor([1/9] * 9).to(x_start.device))
-        missing_r = torch.nonzero(mask[0, 0] == 0).long().reshape(-1)
+        missing_r = torch.nonzero(mask[0, 0].reshape(-1) == 0).long().reshape(-1)
         missing_g = missing_r + 1
         missing_b = missing_g + 1
         missing = torch.cat([missing_r, missing_g, missing_b], dim=0)
