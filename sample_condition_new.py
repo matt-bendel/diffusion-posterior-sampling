66d5bf92912068b37b4c16f036aed10fd08d4b06
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-12 15:22:06 -0400

    Noise update

--- sample_condition_new.py
+++ sample_condition_new.py
@@ -134,7 +134,7 @@ def main():
                     x_start = torch.randn(ref_img.shape, device=device)
                     sample = sample_fn(x_start=x_start, measurement=y_n, record=False, save_root=out_path, mask=mask, noise_sig=measure_config['noise']['sigma'])
 
-                sample = ref_img * mask + (1 - mask) * sample
+                # sample = ref_img * mask + (1 - mask) * sample
                 # plt.imsave(os.path.join(out_path, 'input', fname), clear_color(y_n))
                 # plt.imsave(os.path.join(out_path, 'label', fname), clear_color(ref_img))
                 for j in range(sample.shape[0]):
