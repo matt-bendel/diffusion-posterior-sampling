8155d3fceda5358dfe45e3c5ca551fe01a9a19cb
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-13 11:01:32 -0400

    Noise update

--- sample_condition_new.py
+++ sample_condition_new.py
@@ -102,13 +102,13 @@ def main():
     operators = ['sr_bicubic4', 'sr_bicubic8', 'blur_uni', 'blur_gauss', 'blur_aniso', 'color', 'sr4', 'sr8', 'denoising']
     noise_levels = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 1]
 
-    operators = ['color', 'sr4', 'sr8',
-                 'denoising']
-    noise_levels = [0.05, 0.05, 0.05, 1]
+    operators = ['denoising']
+    noise_levels = [1]
 
     for l in range(len(operators)):
         measure_config['noise']['sigma'] = noise_levels[l]
         measure_config['operator']['name'] = operators[l]
+        noiser = get_noise(**measure_config['noise'])
 
         base_im_count = 0
         for i, data in enumerate(test_loader):
