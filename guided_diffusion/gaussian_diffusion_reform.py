9631356a2d405ef7c4468cc504769522c3de92a3
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-12 20:12:23 -0400

    Noise update

--- guided_diffusion/gaussian_diffusion_reform.py
+++ guided_diffusion/gaussian_diffusion_reform.py
@@ -213,7 +213,7 @@ class GaussianDiffusion:
             pdf = lambda x: torch.exp(torch.Tensor([-0.5 * (x / sigma) ** 2]))
             kernel1 = torch.Tensor([pdf(-4), pdf(-3), pdf(-2), pdf(-1), pdf(0), pdf(1), pdf(2), pdf(3), pdf(4)]).to(
                 x_start.device)
-            H = Deblurring2D(kernel1 / kernel1.sum(), kernel2 / kernel2.sum(), x_start.shape[1], x_start.shape[2], x_start.device)
+            svd = Deblurring2D(kernel1 / kernel1.sum(), kernel2 / kernel2.sum(), x_start.shape[1], x_start.shape[2], x_start.device)
         else:
             svd = Denoising(x_start.shape[1], x_start.shape[2], x_start.device)
 
