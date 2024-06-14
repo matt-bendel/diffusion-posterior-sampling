f9dbd219de7dc03c20f9902415cccb5e976558ad
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-14 13:24:36 -0400

    Noise update

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -40,10 +40,10 @@ class VAMP:
 
         v_min = ((1 - torch.tensor(self.alphas_cumprod).to(noisy_im.device)) / torch.tensor(self.alphas_cumprod).to(noisy_im.device))[0].float()
 
-        delta = noise_var.clone() / v_min
-        delta[delta > 1] = 1.
-        noise_var[delta < 1] = v_min
-        q = 0
+        # delta = noise_var.clone() / v_min
+        # delta[delta > 1] = 1.
+        # noise_var[delta < 1] = v_min
+        # q = 0.5
 
         t = nearest_indices
         print(f'{noise_var[0]};{t[0]}')
@@ -54,9 +54,9 @@ class VAMP:
         if noise_predict.shape[1] == 2 * noisy_im.shape[1]:
             noise_predict, _ = torch.split(noise_predict, noisy_im.shape[1], dim=1)
 
-        noise_to_remove = ((delta ** q) * torch.sqrt(noise_var))[:, 0, None, None, None] * noise_predict
-        x_0 = noisy_im - noise_to_remove
-        # x_0 = noisy_im - torch.sqrt(noise_var)[:, 0, None, None, None] * noise_predict
+        # noise_to_remove = ((delta ** q) * torch.sqrt(noise_var))[:, 0, None, None, None] * noise_predict
+        # x_0 = noisy_im - noise_to_remove
+        x_0 = noisy_im - 10 * torch.sqrt(noise_var)[:, 0, None, None, None] * noise_predict
 
         return x_0
 
