01ec04eb00690c47a882f03db6ac6ec2c742e694
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-17 19:25:24 -0400

    block-diag

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -47,6 +47,7 @@ class VAMP:
 
 
         print(1/eta[0])
+        print(eta[0])
         print((1 / noise_sig ** 2) + r_sig_inv ** 2 + gam1[0])
         print(r_sig_inv ** 2 + gam1[0])
         print(gam1[0])
