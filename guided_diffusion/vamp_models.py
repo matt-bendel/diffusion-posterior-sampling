aff15ea9a4c2ad85dc80483fcd7e62cd616ec6f7
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-17 19:44:27 -0400

    block-diag

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -41,10 +41,10 @@ class VAMP:
         singulars = singulars.reshape(gamma_1.shape[0], -1, 3).permute(0, 2, 1).reshape(gamma_1.shape[0], -1).view(gamma_1.shape[0], 3, 256, 256)
 
         # TODO: Handle case when V is not identity...
-        print(gamma_1.shape)
-        print(singulars.shape)
+        print(singulars[0, 0, 102:106, 78:82])
+
         diag_mat_inv = ((singulars / noise_sig) ** 2 + r_sig_inv ** 2 + gamma_1) ** -1
-        print(diag_mat_inv[0, 0, 102:106, 78:82])
+        # print(diag_mat_inv[0, 0, 102:106, 78:82])
 
         eta = torch.zeros(gamma_1.shape[0], self.Q).to(gamma_1.device)
         for q in range(self.Q):
