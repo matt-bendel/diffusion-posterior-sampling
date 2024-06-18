dba19f1c0922210424af32fdba5dc4cc6c698cdc
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-17 20:08:13 -0400

    block-diag

--- guided_diffusion/ddrm_svd.py
+++ guided_diffusion/ddrm_svd.py
@@ -60,7 +60,7 @@ class H_functions:
         singulars = self.singulars()
         return self.V(self.add_zeros(singulars * temp[:, :singulars.shape[0]]))
 
-    def vamp_mu_1(self, vec, sig_y, sig_ddpm, gamma_1, Q):
+    def vamp_mu_1(self, vec, sig_y, sig_ddpm, gamma_1):
         temp = self.Vt(vec)
         evals = self.add_zeros((self.singulars().unsqueeze(0).repeat(vec.shape[0], 1) / sig_y) ** 2)
         reshape_gam_1 = gamma_1.clone().reshape(vec.shape[0], self.channels, -1).permute(0, 2, 1).reshape(vec.shape[0], -1)
