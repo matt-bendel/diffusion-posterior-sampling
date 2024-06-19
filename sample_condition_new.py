a1fbc578be94e9c4e0159b25d4d38878b3dc9520
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-19 12:01:39 -0400

    block-diag

--- sample_condition_new.py
+++ sample_condition_new.py
@@ -201,7 +201,7 @@ def main():
             # y_n = ref_img
             y_n = noiser(y_n)
 
-            for k in range(4):
+            for k in range(8):
                 # Sampling
                 with torch.no_grad():
                     x_start = torch.randn(ref_img.shape, device=device)
