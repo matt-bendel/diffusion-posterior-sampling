d6026e5c77e37af8ebd4d0475381e583c54142a2
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-23 13:05:44 -0400

    block-diag

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -138,7 +138,7 @@ class VAMP:
 
         t_alpha_bar = extract_and_expand(self.alphas_cumprod, t, x_t)[0, 0, 0, 0]
 
-        for i in range(1):
+        for i in range(2):
             old_gamma_1 = gamma_1
             old_r_1 = r_1
 
