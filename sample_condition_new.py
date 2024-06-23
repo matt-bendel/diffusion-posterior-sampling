ff4ba21e502c597f6752a693728948be8cfc1a91
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-23 14:55:12 -0400

    block-diag

--- sample_condition_new.py
+++ sample_condition_new.py
@@ -105,7 +105,7 @@ def main():
     noise_levels = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 1]
 
     operators = ['inpainting']
-    noise_levels = [0.1]
+    noise_levels = [0.2]
 
     for l in range(len(operators)):
         measure_config['noise']['sigma'] = noise_levels[l]
