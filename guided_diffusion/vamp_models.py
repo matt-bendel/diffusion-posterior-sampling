7e06a66a410ecf7e4851d7c134db54cdbfe1e470
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-26 16:53:17 -0400

    Testing

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -245,9 +245,10 @@ class VAMP:
                 print(gamma_2_raw)
 
                 new_r_2 = torch.zeros(r_2.shape).to(r_2.device)
+                max_g_2, _ = torch.max(1/gamma_2, dim=1, keepdim=True)
                 for q in range(self.Q):
                     new_r_2 += (r_2 + torch.randn_like(r_2).to(r_2.device) * (
-                                1 / gamma_2[:, q] - 1 / gamma_2_raw[:, q]).sqrt()) * self.mask[q, None, :, :, :]
+                                max_g_2 - 1 / gamma_2_raw[:, q]).sqrt()) * self.mask[q, None, :, :, :]
 
                 r_2 = new_r_2
 
