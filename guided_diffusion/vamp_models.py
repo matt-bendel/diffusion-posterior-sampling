ee211a7a26f4e47226b6269c9a5602724dab712f
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-27 10:57:14 -0400

    Testing

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -247,8 +247,8 @@ class VAMP:
 
             print(f'eta_1 = {eta_1[0].cpu().numpy()}; eta_2 = {eta_2[0].cpu().numpy()}; gamma_1 = {gamma_1[0].cpu().numpy()}; gamma_2 = {gamma_2[0].cpu().numpy()}; gamma_1 + gamma_2 = {(gamma_1 + gamma_2)[0].cpu().numpy()}')
 
-            # plt.imsave(f'mu_1.png', clear_color(mu_1))
-            # plt.imsave(f'mu_2.png', clear_color(mu_2))
+            plt.imsave(f'mu_1.png', clear_color(mu_1))
+            plt.imsave(f'mu_2.png', clear_color(mu_2))
 
             if torch.isnan(gamma_2).any(1).any(0) or torch.isnan(gamma_1).any(1).any(0):
                 exit()
