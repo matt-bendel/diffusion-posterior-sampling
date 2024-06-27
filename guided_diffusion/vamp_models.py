e833316532a9eecdebb51aacb54c0d1fadda4d6f
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-27 13:00:07 -0400

    Testing

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -246,7 +246,7 @@ class VAMP:
                 r_2 = new_r_2
 
             print(f'eta_1 = {eta_1[0].cpu().numpy()}; eta_2 = {eta_2[0].cpu().numpy()}; gamma_1 = {gamma_1[0].cpu().numpy()}; gamma_2 = {gamma_2[0].cpu().numpy()}; gamma_1 + gamma_2 = {(gamma_1 + gamma_2)[0].cpu().numpy()}')
-
+            exit()
             plt.imsave(f'mu_1.png', clear_color(mu_1))
             plt.imsave(f'mu_2.png', clear_color(mu_2))
 
