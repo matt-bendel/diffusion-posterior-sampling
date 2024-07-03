c27cc7a379fe17a55dbc3cecc893ded080cf2286
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-03 10:37:02 -0400

    Image colorization

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -273,6 +273,7 @@ class VAMP:
             print(
                 f'eta_1 = {eta_1[0].cpu().numpy()}; eta_2 = {eta_2[0].cpu().numpy()}; gamma_1 = {gamma_1[0].cpu().numpy()}; gamma_2 = {gamma_2[0].cpu().numpy()}; gamma_1 + gamma_2 = {(gamma_1 + gamma_2)[0].cpu().numpy()}')
 
+            plt.imsave('mu_1.png', clear_color(mu_1))
             # if torch.isnan(gamma_2).any(1).any(0) or torch.isnan(gamma_1).any(1).any(0):
             #     exit()
 
--- sample_condition_new.py
+++ sample_condition_new.py
@@ -105,7 +105,7 @@ def main():
     operators = ['sr_bicubic4', 'sr_bicubic8', 'blur_uni', 'blur_gauss', 'blur_aniso', 'color', 'sr4', 'sr8', 'denoising']
     noise_levels = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 1]
 
-    operators = ['color']
+    operators = ['inpainting']
     noise_levels = [0.01]
 
     for l in range(len(operators)):
