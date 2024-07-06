e0f7468c41b304d7f40be8cee113b5bc53b53890
Matt Bendel <mattbendel@Matts-MBP-3.attlocal.net>
2024-07-06 11:28:05 -0400

    Image colorization

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -94,6 +94,7 @@ class VAMP:
         else:
             singulars = singulars.reshape(gamma_1.shape[0], -1).view(gamma_1.shape[0], 3, 256, 256)
 
+        print(singulars[0, :, 0, 0])
         diag_mat_inv = ((singulars / noise_sig) ** 2 + r_sig_inv ** 2 + gamma_1) ** -1
 
         eta = torch.zeros(gamma_1.shape[0], self.Q).to(gamma_1.device)
@@ -325,9 +326,9 @@ class VAMP:
                 #     old_gamma_2 ** (-1 / 2)) ** -2
 
                 # if i > 0:
-                gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
-                       old_gamma_1 ** (-1 / 2)) ** -2
-                r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
+                # gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
+                #        old_gamma_1 ** (-1 / 2)) ** -2
+                # r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
 
                 gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
                            old_gamma_2 ** (-1 / 2)) ** -2
