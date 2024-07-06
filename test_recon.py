baa38836a219155b5c2ae3bc8cb763baae47f041
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-06 15:09:05 -0400

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -257,8 +257,8 @@ def main():
                     t_vals = [0, 25, 50, 100, 250, 500, 750, 999]
                     # t_vals = [25, 50, 100, 250]
                     # damping_factos = [0.1, 0.2, 0.5, 0.75, 1]
-                    t_vals = [250]
-                    damping_factos = [0.1]
+                    # t_vals = [250]
+                    damping_factos = [0.01]
                     for damp in damping_factos:
                         vamp_model.damping_factor = damp
                         for t in t_vals:
