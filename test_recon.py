3fc6e6299bcc49f8efb9eb1edc5ff4a7daf73219
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-06 19:33:04 -0400

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -284,7 +284,7 @@ def main():
                             plt.xlabel('VAMP Iteration')
                             plt.legend(['1/eta_1', '1/eta_2', '1/gam_1', '1/gam_2', 'MSE mu_1', 'MSE mu_2'])
                             plt.title(measure_config['operator']['name'])
-                            plt.savefig(f'vamp_debug/regular_damping/{measure_config["operator"]["name"]}/trajectories_t={t}_damp={damp}.png')
+                            plt.savefig(f'vamp_debug/{measure_config["operator"]["name"]}/trajectories_t={t}_damp={damp}.png')
                             plt.close()
 
             break
