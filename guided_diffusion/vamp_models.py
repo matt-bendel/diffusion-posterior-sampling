12177e0c9a66a1a7f42c9543fdfef05a7ec83a9c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-05 20:23:45 -0400

    Image colorization

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -324,10 +324,10 @@ class VAMP:
                 # gamma_2 = (damp_fac * gamma_2_raw ** (-1 / 2) + (1 - damp_fac) *
                 #     old_gamma_2 ** (-1 / 2)) ** -2
 
-                if i > 0:
-                    gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
-                           old_gamma_1 ** (-1 / 2)) ** -2
-                    r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
+                # if i > 0:
+                gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
+                       old_gamma_1 ** (-1 / 2)) ** -2
+                r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
 
                 gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
                            old_gamma_2 ** (-1 / 2)) ** -2
