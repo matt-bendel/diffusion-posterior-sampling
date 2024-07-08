d7ad0fb05d5d60a37f103c579f6352dd61f71bdf
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-08 18:06:31 -0400

    Image colorization

--- guided_diffusion/vamp_models.py
+++ guided_diffusion/vamp_models.py
@@ -352,12 +352,12 @@ class VAMP:
                 # gamma_2_raw = gamma_2.clone().abs()
                 # gamma_2 = (damp_fac * gamma_2_raw ** (-1 / 2) + (1 - damp_fac) * old_gamma_2 ** (-1 / 2)) ** -2
 
-                if i > 0:
-                    gamma_1_raw = gamma_1.clone()
-                    gamma_1 = (damp_fac * gamma_1_raw ** (-1 / 2) + (1 - damp_fac) *
-                       old_gamma_1 ** (-1 / 2)) ** -2
-                    r_1 = r_1 + torch.randn_like(r_2).to(r_2.device) * (1/gamma_1 - 1/gamma_1_raw).sqrt()[:, 0]
-                    r_2 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
+                # if i > 0:
+                gamma_1_raw = gamma_1.clone()
+                gamma_1 = (damp_fac * gamma_1_raw ** (-1 / 2) + (1 - damp_fac) *
+                   old_gamma_1 ** (-1 / 2)) ** -2
+                r_1 = r_1 + torch.randn_like(r_2).to(r_2.device) * (1/gamma_1 - 1/gamma_1_raw).sqrt()[:, 0]
+                r_2 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
 
                 gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
                            old_gamma_2 ** (-1 / 2)) ** -2
