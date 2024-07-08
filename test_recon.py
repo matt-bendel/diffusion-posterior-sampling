a3cb5cdd2b24efb92f23e34f01507048598c1617
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-08 00:25:03 -0400

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -109,7 +109,7 @@ def main():
     operators = ['sr_bicubic8', 'color', 'inpainting']
     noise_levels = [0.01, 0.01, 0.01, 0.01]
 
-    operators = ['sr8']
+    operators = ['sr4']
     noise_levels = [0.01]
 
     for l in range(len(operators)):
