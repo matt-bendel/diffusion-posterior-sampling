096a849e51cc85a82bc148efe32535a93ef53ec1
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-10 01:28:52 -0400

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -272,7 +272,7 @@ def main():
                             mser2s = []
 
                             x_t = sampler.q_sample(x_start, t)
-                            _, eta1s, eta2s, gam1s, gam2s, mu1s, mu2s, r1s, r2s = vamp_model.run_vamp_reverse_test(x_t, y, torch.tensor([t]).to(x_t.device), measure_config['noise']['sigma'], measure_config["operator"]["name"], ref_img, True)
+                            _, eta1s, eta2s, gam1s, gam2s, mu1s, mu2s, r1s, r2s = vamp_model.run_vamp_reverse_test(x_t, y, torch.tensor([t]).to(x_t.device), measure_config['noise']['sigma'], measure_config["operator"]["name"], ref_img, False)
 
                             for out in mu1s:
                                 mse1s.append(torch.nn.functional.mse_loss(ref_img, out).item())
