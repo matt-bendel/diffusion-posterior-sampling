45c4e88e03a109beb3f77be8cdb2da767d39aec7
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-17 10:36:24 -0600

    Image colorization

--- guided_diffusion/vamp_models_subspace.py
+++ guided_diffusion/vamp_models_subspace.py
@@ -213,13 +213,13 @@ class VAMP:
             plt.imsave(f'vamp_debug/{prob_name}/denoise_in/denoise_in_t={t[0].cpu().numpy()}_vamp_iter={i}.png', clear_color(self.svd.V(r_2).view(r_2.shape[0], 3, 256, 256)))
 
             r_1, gamma_1, eta_2, mu_2, noise_var, true_noise_var = self.denoising(r_2, gamma_2, t, vamp_iter=i, gt=gt)
-            if use_damping:
-                damp_fac = self.damping_factor
-
-                if i > 1:
-                    gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
-                               old_gamma_1 ** (-1 / 2)) ** -2
-                    r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
+            # if use_damping:
+            #     damp_fac = self.damping_factor
+            #
+            #     if i > 1:
+            #         gamma_1 = (damp_fac * gamma_1 ** (-1 / 2) + (1 - damp_fac) *
+            #                    old_gamma_1 ** (-1 / 2)) ** -2
+            #         r_1 = damp_fac * r_1 + (1 - damp_fac) * old_r_1
 
             mu_1, r_2, gamma_2, eta_1 = self.linear_estimation(r_1, gamma_1, x_t / torch.sqrt(1 - t_alpha_bar),
                                                                y / noise_sig,
@@ -228,8 +228,8 @@ class VAMP:
 
             plt.imsave(f'vamp_debug/{prob_name}/denoise_in_pre_damp/denoise_in_t={t[0].cpu().numpy()}_vamp_iter={i}.png', clear_color(self.svd.V(r_2).view(r_2.shape[0], 3, 256, 256)))
 
-            if use_damping:
-                damp_fac = self.damping_factor
+            # if use_damping:
+            #     damp_fac = self.damping_factor
 
                 # gamma_2_raw = gamma_2.clone().abs()
                 # gamma_2 = (damp_fac * gamma_2_raw ** (-1 / 2) + (1 - damp_fac) * old_gamma_2 ** (-1 / 2)) ** -2
@@ -239,9 +239,9 @@ class VAMP:
                 #         (1 / gamma_2 - 1 / gamma_2_raw), torch.zeros(gamma_2.shape).to(gamma_2.device)).sqrt()[:, 1])[:,
                 #                                   singulars.shape[0]:]
 
-                gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
-                           old_gamma_2 ** (-1 / 2)) ** -2
-                r_2 = damp_fac * r_2 + (1 - damp_fac) * old_r_2
+                # gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
+                #            old_gamma_2 ** (-1 / 2)) ** -2
+                # r_2 = damp_fac * r_2 + (1 - damp_fac) * old_r_2
 
             eta1s.append(1/eta_1[0, 1].cpu().numpy())
             eta2s.append(1/eta_2[0, 1].cpu().numpy())
