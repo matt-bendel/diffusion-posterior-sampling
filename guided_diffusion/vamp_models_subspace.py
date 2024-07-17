b7f78f293a7062feb3dea266159b4bfbcba0ca49
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-17 10:39:27 -0600

    Image colorization

--- guided_diffusion/vamp_models_subspace.py
+++ guided_diffusion/vamp_models_subspace.py
@@ -130,13 +130,11 @@ class VAMP:
         r_2 = torch.zeros(mu_1.shape).to(mu_1.device)
         noise = torch.randn_like(r_2)
         # noise = torch.zeros(mu_1.shape).to(mu_1.device)
-        print(gamma_2)
-        exit()
         r_2[:, :singulars.shape[0]] = ((eta_1[:, 0, None] * mu_1 - gamma_1[:, 0, None] * r_1) / gamma_2[:, 0, None] + noise * (max_g_2 - 1/gamma_2[:, 0]).sqrt())[:, :singulars.shape[0]]
         if self.Q > 1:
             r_2[:, singulars.shape[0]:] = ((eta_1[:, 1, None] * mu_1 - gamma_1[:, 1, None] * r_1) / gamma_2[:, 1,None] + noise * (max_g_2 - 1/gamma_2[:, 1]).sqrt())[:, singulars.shape[0]:]
 
-        gamma_2 = max_g_2.unsqueeze(1).repeat(1, self.Q)
+        gamma_2 = 1/max_g_2.unsqueeze(1).repeat(1, self.Q)
 
         return mu_1, r_2, gamma_2, eta_1
 
