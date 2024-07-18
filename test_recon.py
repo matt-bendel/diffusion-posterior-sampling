e54fcd381c04ff0d8d0d4714f489961fdafc4f83
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-17 19:33:52 -0600

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -106,11 +106,11 @@ def main():
     operators = ['sr_bicubic4', 'sr_bicubic8', 'blur_uni', 'blur_gauss', 'blur_aniso', 'color', 'sr4', 'sr8', 'denoising']
     noise_levels = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 1]
 
-    # operators = ['sr_bicubic8', 'color', 'inpainting']
-    # noise_levels = [0.01, 0.01, 0.01, 0.01]
-
-    operators = ['sr_bicubic8']
-    noise_levels = [0.01]
+    operators = ['sr_bicubic8', 'color', 'inpainting']
+    noise_levels = [0.01, 0.01, 0.01]
+    #
+    # operators = ['sr_bicubic8']
+    # noise_levels = [0.01]
 
     for l in range(len(operators)):
         measure_config['noise']['sigma'] = noise_levels[l]
@@ -260,7 +260,7 @@ def main():
                     t_vals = [0, 25, 50, 100, 250, 500, 750, 999]
                     # t_vals = [25, 50, 100, 250]
                     # damping_factos = [0.1, 0.2, 0.5, 0.75, 1]
-                    t_vals = [999]
+                    # t_vals = [999]
                     damping_factos = [0.1]
                     for damp in damping_factos:
                         vamp_model.damping_factor = damp
