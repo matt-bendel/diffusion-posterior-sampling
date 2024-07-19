aa245469ed5700d322c21a3b3d857c2ce2c438bf
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-19 11:36:02 -0400

    Image colorization

--- guided_diffusion/vamp_models_subspace.py
+++ guided_diffusion/vamp_models_subspace.py
@@ -235,17 +235,17 @@ class VAMP:
             if use_damping:
                 damp_fac = self.damping_factor
 
-                # gamma_2_raw = gamma_2.clone().abs()
-                # gamma_2 = (damp_fac * gamma_2_raw ** (-1 / 2) + (1 - damp_fac) * old_gamma_2 ** (-1 / 2)) ** -2
-                # r_2[:, :singulars.shape[0]] = (r_2 + torch.randn_like(r_2).to(r_2.device) * torch.maximum((1 / gamma_2 - 1 / gamma_2_raw), torch.zeros(gamma_2.shape).to(gamma_2.device)).sqrt()[:, 0])[:, :singulars.shape[0]]
-                # if self.Q > 1:
-                #     r_2[:, singulars.shape[0]:] = (r_2 + torch.randn_like(r_2).to(r_2.device) * torch.maximum(
-                #         (1 / gamma_2 - 1 / gamma_2_raw), torch.zeros(gamma_2.shape).to(gamma_2.device)).sqrt()[:, 1])[:,
-                #                                   singulars.shape[0]:]
+                gamma_2_raw = gamma_2.clone().abs()
+                gamma_2 = (damp_fac * gamma_2_raw ** (-1 / 2) + (1 - damp_fac) * old_gamma_2 ** (-1 / 2)) ** -2
+                r_2[:, :singulars.shape[0]] = (r_2 + torch.randn_like(r_2).to(r_2.device) * torch.maximum((1 / gamma_2 - 1 / gamma_2_raw), torch.zeros(gamma_2.shape).to(gamma_2.device)).sqrt()[:, 0])[:, :singulars.shape[0]]
+                if self.Q > 1:
+                    r_2[:, singulars.shape[0]:] = (r_2 + torch.randn_like(r_2).to(r_2.device) * torch.maximum(
+                        (1 / gamma_2 - 1 / gamma_2_raw), torch.zeros(gamma_2.shape).to(gamma_2.device)).sqrt()[:, 1])[:,
+                                                  singulars.shape[0]:]
 
-                gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
-                           old_gamma_2 ** (-1 / 2)) ** -2
-                r_2 = damp_fac * r_2 + (1 - damp_fac) * old_r_2
+                # gamma_2 = (damp_fac * gamma_2 ** (-1 / 2) + (1 - damp_fac) *
+                #            old_gamma_2 ** (-1 / 2)) ** -2
+                # r_2 = damp_fac * r_2 + (1 - damp_fac) * old_r_2
 
             # if torch.linalg.norm(mu_1 - mu_2).cpu().numpy() > 5e3:
             #     break
