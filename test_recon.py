a88e656b9d9953e9aa7eed959d7ecf7acc091b22
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-19 11:27:35 -0400

    Image colorization

--- test_recon.py
+++ test_recon.py
@@ -332,19 +332,20 @@ def main():
 
                             if vamp_model.Q > 1:
                                 plt.figure()
-                                # plt.semilogy(np.arange(len(eta1s[0])), eta1s[1], color='red')
+                                plt.semilogy(np.arange(len(eta1s[0])), eta1s[1], color='red')
                                 # plt.semilogy(np.arange(len(eta1s[0])), eta2s[1], color='blue')
                                 # plt.semilogy(np.arange(len(eta1s[0])), gam1s[1], color='green')
                                 # plt.semilogy(np.arange(len(eta1s[0])), gam2s[1], color='orange')
                                 plt.semilogy(np.arange(len(eta1s[0])), mse12s, linestyle='dashed', color='red')
-                                print(mse12s)
                                 # plt.semilogy(np.arange(len(eta1s[0])), mse22s, linestyle='dashed', color='blue')
                                 # plt.semilogy(np.arange(len(eta1s[0])), mser12s, linestyle='dashed', color='green')
                                 # plt.semilogy(np.arange(len(eta1s[0])), mser22s, linestyle='dashed', color='orange')
                                 plt.xlabel('VAMP Iteration')
+                                # plt.legend(
+                                #     ['1/eta_1', '1/eta_2', '1/gam_1', '1/gam_2', 'MSE mu_1', 'MSE mu_2', 'MSE r_1',
+                                #      'MSE r_2'])
                                 plt.legend(
-                                    ['1/eta_1', '1/eta_2', '1/gam_1', '1/gam_2', 'MSE mu_1', 'MSE mu_2', 'MSE r_1',
-                                     'MSE r_2'])
+                                    ['1/eta_1', 'MSE mu_1'])
                                 plt.title(f"{measure_config['operator']['name']}; nonmeasured subspace")
                                 plt.savefig(
                                     f'vamp_debug/{measure_config["operator"]["name"]}/trajectories_t={t}_damp={damp}_q=1.png')
