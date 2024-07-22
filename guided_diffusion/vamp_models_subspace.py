002b85e6d7d902a8b35347a76fbe612ec3bc3c20
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-22 09:53:05 -0400

    Image colorization

--- guided_diffusion/vamp_models_subspace.py
+++ guided_diffusion/vamp_models_subspace.py
@@ -210,7 +210,7 @@ class VAMP:
         r1s = [[], []]
         r2s = [[], []]
 
-        for i in range(5 if t[0] > 800 else (4 if t[0] > 600 else (3 if t[0] > 400 else 1))):
+        for i in range(1):
             old_gamma_1 = gamma_1.clone()
             old_gamma_2 = gamma_2.clone()
 
