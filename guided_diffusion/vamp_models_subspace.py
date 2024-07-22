0418f62cdd3838e26443311fdaa99add83db5e78
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-22 16:38:54 -0400

    Image colorization

--- guided_diffusion/vamp_models_subspace.py
+++ guided_diffusion/vamp_models_subspace.py
@@ -219,7 +219,7 @@ class VAMP:
         r1s = [[], []]
         r2s = [[], []]
 
-        for i in range(5):
+        for i in range(10):
             self.rho = self.rho_schedule[i]
             old_gamma_1 = gamma_1.clone()
             old_gamma_2 = gamma_2.clone()
