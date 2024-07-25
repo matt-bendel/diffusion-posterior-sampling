db3339750dcb43b24663716ddf77b99d17a406a6
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-07-25 12:50:30 -0400

    Image colorization

--- guided_diffusion/gaussian_diffusion_vamp_ddim.py
+++ guided_diffusion/gaussian_diffusion_vamp_ddim.py
@@ -335,10 +335,6 @@ def space_timesteps(num_timesteps, section_counts):
     elif isinstance(section_counts, int):
         section_counts = [section_counts]
 
-    # print(section_counts)
-    # print(num_timesteps)
-    # exit()
-
     size_per = num_timesteps // len(section_counts)
     extra = num_timesteps % len(section_counts)
     start_idx = 0
