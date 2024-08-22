83bf563680edb9a125abc69f1402244f899783eb
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-08-22 15:09:16 -0400

    create test set

--- get_test_sets_diffpir.py
+++ get_test_sets_diffpir.py
@@ -1,10 +1,10 @@
 import shutil
 
-initial_im = 69007
-final_im = 69997
+initial_im = 69000
+final_im = 69999
 
 while initial_im != final_im:
     src = f'/storage/FFHQ/ffhq256/0/{initial_im}.png'
-    dst = f'/storage/FFHQ/ffhq256_diffpirtest/0/{initial_im}.png'
+    dst = f'/storage/FFHQ/ffhq256_firetest/0/{initial_im}.png'
     shutil.copyfile(src, dst)
     initial_im += 10
