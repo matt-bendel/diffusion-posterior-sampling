f9225e452330ca320a9bee4f2259d18062b1bddf
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-10-07 22:52:04 -0400

    Added ohayon to plot

--- sample_condition.py
+++ sample_condition.py
@@ -142,6 +142,7 @@ def main():
                            clear_color(sample[j].unsqueeze(0)))
 
             base_im_count += sample.shape[0]
+            exit()
 
         print(f'Avg. LPIPS: {np.mean(lpips_vals)} +/- {np.std(lpips_vals) / len(lpips_vals)}')
         print(f'Avg. PSNR: {np.mean(psnr_vals)} +/- {np.std(psnr_vals) / len(psnr_vals)}')
