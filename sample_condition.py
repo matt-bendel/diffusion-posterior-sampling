9c6ac1230de8786bb935e0c2639751a27f4b6500
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-11-11 16:26:54 -0500

    runtime

--- sample_condition.py
+++ sample_condition.py
@@ -105,10 +105,10 @@ def main():
     for k in range(1):
         base_im_count = 0
         for i, data in enumerate(test_loader):
-            if i < 10:
+            if i < 8:
                 continue
 
-            if i > 30:
+            if i > 15:
                 exit()
             logger.info(f"Inference for image {i}")
             y, x, mask, mean, std = data[0]
